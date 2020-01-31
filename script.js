6e81d6add419506ec233352d33d2cc4f5ab68ac4
ZIYUN-Murasaki <liuz404@newschool.edu>
2020-01-31 15:36:27 -0500

    my god

--- script.js
+++ script.js
@@ -1,22 +1,19 @@
 // $(document).ready(function(){
 
 
-//selecting elements 
-var containerBox = document,getElementById("container-box");
-var box3 = document,getElementById("box3");
+// //selecting elements 
+// var containerBox = document,getElementById("container-box");
+// var box3 = document,getElementById("box3");
 
-//adding event to element on click 
-containerBox,addEventListener("click", handleClick);
+// //adding event to element on click 
+// containerBox,addEventListener("click", handleClick);
 
-//What happens when clicked 
-function handleClick() {
-	box3classList.toggle("open");
-}
-
-
-$('.box3'). on('click', function(){
-	$('.')
-}
+// //What happens when clicked 
+// function handleClick() {
+// 	box3classList.toggle("open");
+// }
 
 
-});
+
+
+
