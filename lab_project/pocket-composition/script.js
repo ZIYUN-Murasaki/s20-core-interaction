aea6a8e761a929ca7207730a1a1f2571fb0688d4
ZIYUN-Murasaki <liuz404@newschool.edu>
2020-02-14 16:00:27 -0500

    c

---
+++ lab_project/pocket-composition/script.js
@@ -0,0 +1,25 @@
+// .parent{
+// 	display: flex;  /* or inline-flex */
+// 	flex direction: row;
+// 	/*row/ column / reverse row/ reverse column*/
+// 	flex-wrap: nowrap;
+// 	justify-content: space-around;
+// 	/*space-between/ space- around/ center/ flex-end*/
+// 	align-items: stretch;
+// 	/*align things in column*/
+// 	/*see the resources on the the lab page*/
+// }
+
+
+
+// .child{
+// 	padding: 1rem;
+// 	margin: 1rem;
+// 	background-color: red;
+// 	color: white;
+// }
+
+// .child:nth-of-type(even){
+// 	background-color: purple;
+// }
+
